358b41c581a5320dd01743ed0ecf73a9e16e66cf
edwinjoel1204 <edwinjoel1204@gmail.com>
2024-09-03 21:21:06 +0530

    HomeController created and index method added

---
+++ src/main/java/com/edwin/todo/controller/HomeController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edwin.todo.controller;
+
+import com.edwin.todo.service.todoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author edwin
+ */
+
+@Controller
+public class HomeController {
+    
+    @Autowired
+    private todoService todoService;
+    
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoService.getAll());
+        
+        return modelAndView;
+    }
+    
+}
