b8672b01e9a6d8f13ced025c05b052b198352dd8
edwinjoel1204 <edwinjoel1204@gmail.com>
2024-09-03 19:10:52 +0530

    New Spring Project Created

---
+++ src/test/java/com/edwin/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
