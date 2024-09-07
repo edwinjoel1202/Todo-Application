3f6017bd200aff0836295ff725f25c328800b661
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-07 13:24:36 +0530

    Mapping of delete added

--- src/main/java/com/edwin/todo/controller/TodoFormController.java
+++ src/main/java/com/edwin/todo/controller/TodoFormController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,5 +30,14 @@ public class TodoFormController {
         }
         todoService.save(todoItem);
         return "redirect:/";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id){
+            todo todoItem = todoService
+                    .getById(id)
+                    .orElseThrow(()->new IllegalArgumentException("Todo Item not found !"));
+            todoService.delete(todoItem);
+            return "redirect:/";        
     }
 }
