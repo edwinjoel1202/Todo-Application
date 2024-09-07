8da465c089d697e80fe93bd283e7837b67c130d1
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-07 12:45:10 +0530

    Bugs fixed related to Model passing as param in create-todo-controller

--- src/main/java/com/edwin/todo/controller/TodoFormController.java
+++ src/main/java/com/edwin/todo/controller/TodoFormController.java
@@ -1,43 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.edwin.todo.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.edwin.todo.model.todo;
 import com.edwin.todo.service.todoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-/**
- *
- * @author edwin
- */
-
 @Controller
 public class TodoFormController {
-    
+
     @Autowired
     private todoService todoService;
-    
+
     @GetMapping("/create-todo")
-    public String showCreateForm() {
+    public String showCreateForm(Model model) {
+        model.addAttribute("todoItem", new todo());
         return "new-todo-item";
     }
-    
+
     @PostMapping("/todo")
-    public String createTodoItem(@Valid todo todoItem, BindingResult result, Model model){
-        todo item = new todo();
-        item.setDescription(todoItem.getDescription());
-        item.setIsComplete(todoItem.getIsComplete());
-        
+    public String createTodoItem(@Valid todo todoItem, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "new-todo-item";
+        }
         todoService.save(todoItem);
         return "redirect:/";
     }
-    
 }
